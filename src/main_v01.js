c69c4f7bfc1807016e945c966be3363ce4239f19
Raj <gamingmodmania@gmail.com>
2024-01-08 20:23:49 +0530

    update mode changer method in js file

--- src/main_v01.js
+++ src/main_v01.js
@@ -1,12 +1,32 @@
 let toggleButton = document.getElementById('Toggle1');
 let htmlEle = document.getElementsByTagName('html')[0];
 
-toggleButton.addEventListener("click", function(){
-    if(toggleButton.checked){
+toggleButton.addEventListener("click", function () {
+    if (toggleButton.checked) {
         htmlEle.classList.add('dark');
         htmlEle.classList.remove('light');
-    } else{
+        localStorage.setItem('theme', 'dark');
+    } else {
+        htmlEle.classList.add('light');
+        htmlEle.classList.remove('dark');
+        localStorage.setItem('theme', 'light');
+    }
+})
+
+function init() {
+    if (localStorage.getItem('theme') === 'dark') {
+        htmlEle.classList.add('dark');
+        htmlEle.classList.remove('light');
+    } else {
         htmlEle.classList.add('light');
         htmlEle.classList.remove('dark');
     }
-})
+}
+
+if (document.readyState === 'interactive') {
+    init();
+} else {
+    document.addEventListener('DOMContentLoaded', () => {
+        init();
+    })
+}
