2b83efc2554ac4d4c5d45f9210f13aaf156bd9bc
bharat sharma <bigoceancs@gmail.com>
2024-01-11 07:25:33 +0530

    popup code and let's talk code  change

--- src/main_v01.js
+++ src/main_v01.js
@@ -1,3 +1,4 @@
+
 let toggleButton = document.getElementById('Toggle1');
 let htmlEle = document.getElementsByTagName('html')[0];
 
@@ -31,3 +32,48 @@ if (document.readyState === 'interactive') {
         init();
     })
 }
+
+
+// Popup code
+
+const Popup = document.getElementById('popup');
+const contactus = document.getElementById('contactbtn');
+let popupstatus = false;
+const close = document.getElementById('close');
+
+contactus.addEventListener('click', function () {
+    Popup.style.display = 'flex'
+})
+
+close.addEventListener('click', function () {
+    Popup.style.display = 'none';
+})
+
+
+// let's talk code 
+function sendData(url, data) {
+    return new Promise((resolve, reject) => {
+        let xhttp = new XMLHttpRequest();
+        xhttp.open('POST', url, true);
+        xhttp.onreadystatechange = () => {
+            if (this.readyState == 4 && this.status == 200) {
+                resolve(this.responseText);
+            } else {
+                reject("Page not found");
+            }
+        }
+        xhttp.send(data);
+    })
+}
+const name = document.getElementById('name');
+const email = document.getElementById('email');
+const message = document.getElementById('message');
+const sendmsg = document.getElementById('sendmsg');
+
+sendmsg.addEventListener('click', function () {
+    sendData("{{url}}", "data").then((data) => {
+        console.log(data);
+    }).catch((err) => {
+        console.error(err);
+    })
+})
